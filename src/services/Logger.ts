ce0e26b8045c346133904b14fb5d48b87d1bc352
Carsten Gräf <evandor@gmail.com>
2024-09-22 10:32:44 +0200

    apps unification

--- src/services/Logger.ts
+++ src/services/Logger.ts
@@ -1,20 +1,5 @@
-const version = import.meta.env.PACKAGE_VERSION
-
-function log(msg: string, level:number) {
-  const gelfMessage = {
-    "version": "1.1",
-    "host": "browser.local",
-    "short_message": msg,
-    "level":level,
-    _mode: process.env.MODE,
-    _version: version,
-    _stage: process.env.TABSETS_STAGE
-  }
-  // api.post("http://graylog.backend:12201/gelf", gelfMessage, {headers: {
-  //     "Content-Type": "application/json"}
-  // }).catch((err) => {
-  //   console.log("error with logging server")
-  // })
+function log(msg: string, level: number) {
+  // no op in bookmrkx
 }
 
 export function useLogger() {
